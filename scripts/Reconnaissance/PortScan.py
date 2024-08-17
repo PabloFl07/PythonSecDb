ad389de395dacd079508addcb0920c3245cb3fda
Pablo Fernández <pabloflc137@gmail.com>
2024-08-17 19:59:48 +0200

    Script PortScan translated

--- scripts/Reconnaissance/PortScan.py
+++ scripts/Reconnaissance/PortScan.py
@@ -19,11 +19,11 @@ class color:
 
 open_sockets= []
 
-# Definimos la acción que ocurrirá cuando hagamos Ctrl + C
+# Define what will occur when we input Ctrl_C
 def def_handler(sig, frame):
     print(f"\n {color.RED}[!] Saliendo... {color.END}")
 
-    # Cerramos los sockets que quedaron abiertos durante el escaneo
+    # Close the sockets that are still open
     for socket in open_sockets:
         socket.close()
 
@@ -33,16 +33,16 @@ def def_handler(sig, frame):
 signal.signal(signal.SIGINT, def_handler) #Ctrl + C
 
 
-# Trabajamos con los datos necesarios como argumentos
-# Con esta librería, el valor de los argumentos se almacena como una instancia de la clase options
+# We work with the necessary data as arguments
+# With this library, the value of the arguments is stored as an instance of the class options
 
 def get_arguments():
     parser = argparse.ArgumentParser(description="Fast TCP Port Scan")
 
-    # | modo de llamada / donde se almacena el valor / Requerido /Descripción |
+    # | Call mode / Where the value is stored / Required / Description |
     parser.add_argument("-t", "--target", dest="target", required=True, help= "Target's Ip to scan (Ex: -t 192.168.1.2)")
 
-    # | modo de llamada / donde se almacena el valor / Requerido / Descripción |
+    # | Call mode / Where the value is stored / Required / Description |
     parser.add_argument("-p", "--port", dest="port", required=True, help= "Port range to scan (Ex: -p )")
     options = parser.parse_args()
 
@@ -51,10 +51,10 @@ def get_arguments():
 
 def create_socket():
 
-    # Socket para direcciones IPv4 y conexiones TPC
+    # Socket for IPv4 directions and TPC conections
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    #Tiempo máximo de espera para la conexión.
+    #Maximum waiting time for connection.
     s.settimeout(0.5)
 
     open_sockets.append(s)
@@ -64,12 +64,12 @@ def port_scanner(port, host):
 
     s = create_socket()
 
-    # Conexión a un host por un puerto, de forma que si esta abierto, la conexión se realiza.
+    # Connection to a host through a port, so that if it is open, the connection is made.
     try:
         s.connect((host, port))
         print(f"[+] Port {port} -{color.GREEN} OPEN {color.END}")
 
-    # Manejamos la opción de que tarde más de lo deseado en realizar la conexión
+    # We handle the option of taking longer than desired to make the connection.
     except (socket.timeout, ConnectionRefusedError):
         pass
 
@@ -82,7 +82,7 @@ def scan_ports(ports, target):
 
     threads = []
 
-    # Escanea los puertos segun los recibidos en la función "parse_ports"
+    # Scan ports as received in the function “parse_ports”.
     for port in ports:
         thread = threading.Thread(target=port_scanner, args=(port, target))
         threads.append(thread)
@@ -92,25 +92,25 @@ def scan_ports(ports, target):
 
 def parse_ports(ports_str):
 
-    # Trabajamos con los puertos recibidos para que python los interprete correctamente segun lo deseado
+    # We work with the received ports so that python interprets them correctly according to what we want.
 
-    if "-" in ports_str: # Rango de puertos / Asignamos una variable a cada estremo del rango y devolvemos el método "in range()"
+    if "-" in ports_str: # Port range / We assign a variable to each end of the range and return the method “in range()”.
         start, end = map(int, ports_str.split("-"))
         return range(start, end+1)
     
-    elif "," in ports_str: # varios puertos / Convertimos los puertos a una lista tras separarlos por cada ,
+    elif "," in ports_str: # Several ports / We convert the ports to a list after separating them by each ”,”
         return map(int, ports_str.split(","))
     
-    else: # Un solo puerto / Convertimos ese único puerto a una lista
+    else: # A single port / We convert that single port to a list
         return [int(ports_str)]
     
 
-# Función principal
+# Main function
 def main():
-    # Obtemenos los datos de los argumentos
+    # Gets us the data of the arguments
     target, ports_str = get_arguments()
     ports =  parse_ports(ports_str)
-    # Escaneo
+    # Scan
     scan_ports(ports, target)
 
 
